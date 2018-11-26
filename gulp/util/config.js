cee76419492a68c56e5ab80df3562c664808d9aa
Hash Skyd <skydhash@gmail.com>
2018-11-26 11:18:09 -0500

    remove 404 errors

--- gulp/util/config.js
+++ gulp/util/config.js
@@ -2,7 +2,7 @@ const config = {
   fileExpression: {
     copy: '**/*.{doc,docx,zip,pdf,js}',
     font: '**/*.{otf,eot,svg,ttf,woff,woff2}',
-    image: '**/*.{jpg,jpeg,gif,svg,png}'
+    image: '**/*.{jpg,jpeg,gif,svg,png,ico}'
   }
 };
 
