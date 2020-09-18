fbbde6d1ba6daa1d9a7783f8f4a160911c0a5211
Holy-Elie Scaide <skydhash@gmail.com>
2020-09-18 19:11:47 -0400

    update publish directory for v2

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,7 +5,6 @@ const preferDefault = m => (m && m.default) || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/hash/code/personal/website/.cache/dev-404-page.js"))),
   "component---src-pages-index-js": hot(preferDefault(require("/Users/hash/code/personal/website/src/pages/index.js")))
 }
 
