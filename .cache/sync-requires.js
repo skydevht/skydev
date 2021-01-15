968f891e6d3eeee02772668c1fa01fdd556e3104
leregi <leregi46@gmail.com>
2021-01-15 10:38:22 -0500

    first commit before startagain to work on this project

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,7 +5,7 @@ const preferDefault = m => (m && m.default) || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": hot(preferDefault(require("/home/zeta/Dev/projects/skydev-web-site/skydev/.cache/dev-404-page.js"))),
-  "component---src-pages-index-js": hot(preferDefault(require("/home/zeta/Dev/projects/skydev-web-site/skydev/src/pages/index.js")))
+  "component---cache-dev-404-page-js": hot(preferDefault(require("C:\\Users\\Reginald Victor\\Documents\\Dev\\projects\\skydev\\.cache\\dev-404-page.js"))),
+  "component---src-pages-index-js": hot(preferDefault(require("C:\\Users\\Reginald Victor\\Documents\\Dev\\projects\\skydev\\src\\pages\\index.js")))
 }
 
